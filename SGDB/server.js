e648b1efc74e9f402a7f2f549315c3a199e59d29
GustavoAyalaSJ <gustavooayala17@gmail.com>
2026-02-24 14:30:30 -0300

    Atualização na URL

--- SGDB/server.js
+++ SGDB/server.js
@@ -11,6 +11,31 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/introduction', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index2.html'));
+});
+
+app.get('/historico_geral', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index3.html'));
+});
+
+app.get('/carteira_digital', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index4.html'));
+});
+
+app.get('/recarga', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index5.html'));
+});
+
+app.get('/app', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index6.html'));
+});
+
+
 app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
