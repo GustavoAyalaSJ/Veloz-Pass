8e030eaf752bf97bb1fa9f748cbc36669861b2b9
GustavoAyalaSJ <gustavooayala17@gmail.com>
2026-03-10 23:19:29 -0300

    Remoção do Dropdown container e alteração no CSS cabeçalho.

--- SGDB/public/src/script5.js
+++ SGDB/public/src/script5.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const listbutton = document.getElementById('list-button');
-    const contentDropdown = document.getElementById('content-dropdown');
-
-    listbutton.addEventListener('click', (event) => {
-        event.stopPropagation();
-
-        contentDropdown.classList.toggle('active');
-    });
-
-    window.addEventListener('click', (event) => {
-        if (contentDropdown.classList.contains('active')) {
-            if (!contentDropdown.contains(event.target)) {
-                contentDropdown.classList.remove('active');
-            }
-        }
-    });
-});
+/* Comentário em breve. */
