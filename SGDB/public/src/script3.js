c2b062b6b77eeec868f8ed451104e8b1c2665460
GustavoAyalaSJ <gustavooayala17@gmail.com>
2026-03-10 23:33:15 -0300

    Correção no CSS e Adição de Função de Sair para icones de sair.

--- SGDB/public/src/script3.js
+++ SGDB/public/src/script3.js
@@ -1,7 +1,32 @@
-/* Comentário JS: JS gerar uma tabela:
+document.addEventListener('DOMContentLoaded', () => {
+
+    const exitLink = document.querySelector('.exit-link');
+    const logoutModal = document.getElementById("logoutModal");
+
+    /* Comentário JS: JS gerar uma tabela:
     <!-- Container onde terá as informações de:
         - Recarga: Informando os valores que foram avaliados corretamente para recarga do cartão.
         - Carteira Digital: Colocar com um fundo vermelho translúcido (recusado), amarelo translúcido (em revisão) e verde translúcido (confirmado). 
     Ele vai ser gerado quando o usuário realizar algumas das informações. 
     --> \
-*/
+    */
+
+    if (exitLink) {
+        exitLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            logoutModal.style.display = "flex";
+        });
+    }
+
+    window.fecharModal = function () {
+        logoutModal.style.display = "none";
+    };
+
+    window.confirmarLogout = function () {
+
+        localStorage.removeItem('nomeUsuario');
+        sessionStorage.clear();
+
+        window.location.href = '/introduction';
+    };
+});
--- SGDB/public/src/script5.js
+++ SGDB/public/src/script5.js
@@ -1,1 +1,26 @@
-/* Comentário em breve. */
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* Comentário em breve. */
+
+    const exitLink = document.querySelector('.exit-link');
+    const logoutModal = document.getElementById("logoutModal");
+
+    if (exitLink) {
+        exitLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            logoutModal.style.display = "flex";
+        });
+    }
+
+    window.fecharModal = function () {
+        logoutModal.style.display = "none";
+    };
+
+    window.confirmarLogout = function () {
+
+        localStorage.removeItem('nomeUsuario');
+        sessionStorage.clear();
+
+        window.location.href = '/introduction';
+    };
+});
