6ae883de21d9c5113fe2a5348346955391a4cd7a
Gustavo Ayala <gustavooayala17@gmail.com>
2026-03-25 18:35:41 -0300

    Update script2.js

--- SGDB/public/src/script2.js
+++ SGDB/public/src/script2.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        const saldo = parseFloat(dadosCarteira.saldo_atual) || 0;
+        const saldo = parseFloat(dadosCarteira.saldo || dadosCarteira.saldo_atual) || 0;
         console.log('Saldo formatado:', saldo);
 
         if (saldoDashboard) {
