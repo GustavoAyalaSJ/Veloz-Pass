0f4ba8361c4cbcd77f005a2af814c4b5138b6ec6
Gustavo Ayala <gustavooayala17@gmail.com>
2026-04-02 18:43:26 -0300

    Atualização no paymentValidator.js

--- SGDB/validators/paymentValidator.js
+++ SGDB/validators/paymentValidator.js
@@ -2,22 +2,24 @@ const Joi = require('joi');
 
 const criarTransfer = Joi.object({
     valor: Joi.number().positive().max(10000).required(),
-    
+
     metodo: Joi.string()
         .uppercase() 
         .valid('DEBITO', 'CREDITO', 'INTERNACIONAL', 'PIX', 'CARTEIRA_DIGITAL')
         .required(),
-        
+
     numCartao: Joi.string()
         .min(13)
         .max(19)
+        .allow('', null)
         .when('metodo', {
             is: Joi.string().valid('PIX', 'CARTEIRA_DIGITAL'), 
-            then: Joi.optional().allow(''),
+            then: Joi.optional(),
             otherwise: Joi.required()
         }),
-        
-    idBandeira: Joi.number().optional().allow(null)
+
+    idBandeira: Joi.number().optional().allow(null),
+    origem: Joi.string().optional()
 });
 
 const criarTransferSchema = criarTransfer;
